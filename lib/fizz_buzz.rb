9b16be05372b98e613da3ba27e750f89f9a0f9d6
Samuel Molinari <samuel@molinari.me>
2018-08-22 22:08:32 +0100

    Buzz matcher

--- lib/fizz_buzz.rb
+++ lib/fizz_buzz.rb
@@ -3,9 +3,14 @@ require "fizz_buzz/version"
 module FizzBuzz
   class << self
     FIZZ_MULTIPLIER = 3
+    BUZZ_MULTIPLIER = 5
 
     def fizz?(n)
       n % FIZZ_MULTIPLIER == 0
+    end
+
+    def buzz?(n)
+      n % BUZZ_MULTIPLIER == 0
     end
   end
 end
--- test/fizz_buzz_test.rb
+++ test/fizz_buzz_test.rb
@@ -16,4 +16,14 @@ class FizzBuzzTest < Minitest::Test
     refute FizzBuzz.fizz?(5)
     refute FizzBuzz.fizz?(13)
   end
+
+  def test_buzz_matcher_is_true_on_multiples_of_5
+    assert FizzBuzz.buzz?(5)
+    assert FizzBuzz.buzz?(10)
+    assert FizzBuzz.buzz?(15)
+
+    refute FizzBuzz.buzz?(1)
+    refute FizzBuzz.buzz?(3)
+    refute FizzBuzz.buzz?(151)
+  end
 end
