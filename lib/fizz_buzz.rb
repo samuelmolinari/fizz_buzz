64383e35bf52af4f979103c8ed6b01d8ff1ba241
Samuel Molinari <samuel@molinari.me>
2018-08-22 22:05:23 +0100

    Fizz matcher

--- lib/fizz_buzz.rb
+++ lib/fizz_buzz.rb
@@ -1,5 +1,11 @@
 require "fizz_buzz/version"
 
 module FizzBuzz
-  # Your code goes here...
+  class << self
+    FIZZ_MULTIPLIER = 3
+
+    def fizz?(n)
+      n % FIZZ_MULTIPLIER == 0
+    end
+  end
 end
--- test/fizz_buzz_test.rb
+++ test/fizz_buzz_test.rb
@@ -4,4 +4,16 @@ class FizzBuzzTest < Minitest::Test
   def test_that_it_has_a_version_number
     refute_nil ::FizzBuzz::VERSION
   end
+
+  def test_fizz_matcher_is_true_on_multiples_of_3
+    assert FizzBuzz.fizz?(3)
+    assert FizzBuzz.fizz?(6)
+    assert FizzBuzz.fizz?(9)
+    assert FizzBuzz.fizz?(15)
+
+    refute FizzBuzz.fizz?(1)
+    refute FizzBuzz.fizz?(2)
+    refute FizzBuzz.fizz?(5)
+    refute FizzBuzz.fizz?(13)
+  end
 end
